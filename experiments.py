4d728cd3bbcc7a83763400d5fd46be1c82bcde9c
Jules Fowler <juliafowlerm@gmail.com>
2021-09-08 16:33:06 -0700

    Extremely hacked up in medias res.

--- experiments.py
+++ experiments.py
@@ -1,4 +1,4 @@
-## -- This will be where I set up and run actual experiments on the testbed
+## -- This w#ill be where I set up and run actual experiments on the testbed
 # -- I think this should also hold all the fiddly bits of how I connect to 
 # to hardware or simulation
 # ideally easy flag between 
@@ -56,14 +56,15 @@ class WavefrontControl:
     """
 
 
-    def __init__(self, wavelength=2500e-9, diameter=1, mirror_shape='circular', 
-                 n_sample=6, n_airy=12, grid=120, wfs_type='PyWFS', 
+    def __init__(self, wavelength=2.6e-6, diameter=1, mirror_shape='circular', 
+                 n_sample=4, n_airy=20, grid=120, wfs_type='PyWFS', 
                  modulation_steps=12, modulation_radius=5, pixels=20,
-                 wfs_camera='noiseless', n_actuators=10, probe=.2, 
-                 regularization=1e-3, control_frequency=1000, leak=1, 
-                 gain=0.5, wfc_control_method='integrator'):
+                 wfs_camera='noiseless', n_actuators=9, probe=.2, 
+                 regularization=1e-3, control_frequency=800, leak=.999, 
+                 gain=0.3, wfc_control_method='integrator'):
         """ Init function to set up the wavefront control loop. """
-
+        
+        self.count = 1
         # Image, sampling, and science parameters
         self.wavelength = wavelength
         self.diameter = diameter
@@ -134,7 +135,8 @@ class WavefrontControl:
             return layers 
 
         if model == 'single':
-            layer = InfiniteAtmosphericLayer(self.pupil_grid, 1.5e-13, 80, 10)
+            cn_squared = Cn_squared_from_fried_parameter(0.12, self.wavelength)
+            layer = InfiniteAtmosphericLayer(self.pupil_grid, cn_squared, 50, 10)
         elif model == 'multilayer':
             layer = MultiLayerAtmosphere(build_multilayer_model(self.pupil_grid))
         else:
@@ -154,12 +156,15 @@ class WavefrontControl:
 
         self.strehl_record = [] 
         for i in range(n_steps):
+            self.count = i
             # update atmosphere layer            
             self.atmospheric_layer.t = i*self.dt
-            #phase= self.atmospheric_layer.phase_for(self.wavelength) #this is in radians, unwrapped
+            phase= self.atmospheric_layer.phase_for(self.wavelength) #this is in radians, unwrapped
+
+            self.wf = Wavefront(self.primary_mirror, wavelength=self.wavelength)
             self.wf = self.atmospheric_layer.forward(self.wf)
-            
             # send it through the DM
+            #self.deformable_mirror.flatten()
             self.wf = self.deformable_mirror.forward(self.wf)
 
             # take wfs image
@@ -170,11 +175,17 @@ class WavefrontControl:
             
             # move to science image
             self.science_wf = self.propagator.forward(self.wf)
+            if self.count%50 == 0:
+                plt.clf()
+                imshow_field(self.science_wf.intensity)
+                plt.show()
+                plt.clf()
 
             # save strehl, phase screen, science image, new DM command, WFS image
             self.strehl = get_strehl_from_focal(self.science_wf.intensity, self.reference_image.intensity)
             self.strehl_record.append(self.strehl)
             print(f'Strehl at iter {i}: {self.strehl}')
+
         plt.plot(np.arange(0, n_steps), self.strehl_record)
         plt.savefig('strehl.png')
         plt.clf()
@@ -201,14 +212,15 @@ class WavefrontControl:
                 for mod in range(self.modulation['steps']):
                     self.camera.integrate(wf_wavefront_sensor[mod], 1)
                 
-                response_slopes = self._calculate_wavefront_sensor_slopes(subtract_reference=False)
+                response_slopes = self._calculate_wavefront_sensor_slopes(subtract_reference=False, plot=False)
                 slope += sign * (response_slopes/2*self.probe*self.wavelength)
             
             response.append(slope.ravel())
         
         modal_response = ModeBasis(response)
         
-        self.command_matrix = inverse_tikhonov(modal_response, rcond=self.regularization)
+        self.command_matrix = inverse_tikhonov(modal_response.transformation_matrix, rcond=self.regularization)
+        self.deformable_mirror.flatten()
 
     def _build_deformable_mirror(self):
         """ Function to build a defomrable mirror."""
@@ -223,7 +235,9 @@ class WavefrontControl:
         """ Function to build a primary mirror."""
         
         if self.mirror_shape == 'circular':
-            primary_mirror = evaluate_supersampled(circular_aperture(self.diameter), self.pupil_grid, self.n_sample)
+            #primary_mirror = evaluate_supersampled(circular_aperture(self.diameter), self.pupil_grid, self.n_sample)
+            primary_mirror = circular_aperture(self.diameter) 
+            primary_mirror = primary_mirror(self.pupil_grid)
         
         else:
             raise NotImplementedError("Nothing for that mirror type yet sorry.")
@@ -256,18 +270,34 @@ class WavefrontControl:
         else:
             raise NotImplementedError("Nothing for that WFS type yet sorry.")
         
-    def _calculate_wavefront_sensor_slopes(self, subtract_reference=True):
+    def _calculate_wavefront_sensor_slopes(self, subtract_reference=True,
+            plot=True):
         """ Calculate slopes from WFS. """
         
         def bin_image(wfs_image, n_bins):
             """ Helper function to bin image. """
             # I feel like this might be built into scipy.griddata? 
+            if plot and self.count%50 == 0:
+                print('Unbinned image.')
+                plt.clf()
+                plt.imshow(wfs_image)
+                plt.colorbar()
+                plt.show()
+                plt.clf()
             binned_image = np.zeros((int(wfs_image.shape[0]/n_bins), int(wfs_image.shape[1]/n_bins)))
             for m in np.arange(n_bins-1, wfs_image.shape[0]-n_bins, n_bins):
                 for n in np.arange(n_bins-1, wfs_image.shape[1]-n_bins, n_bins):
                     binned_image[int((m+1)/n_bins)-1, int((n+1)/n_bins)-1] = \
                                  np.sum(wfs_image[m-int((n_bins-1)/2):m+int((n_bins-1)/2),
                                                   n-int((n_bins-1)/2):n+int((n_bins-1)/2)])
+            
+            if plot and self.count%50 == 0:
+                print('Binned image.')
+                plt.clf()
+                plt.imshow(binned_image)
+                plt.colorbar()
+                plt.show()
+                plt.clf()
             return binned_image.flatten()/binned_image.sum()
         
         binned_image = bin_image(self.camera.read_out().shaped, self.n_bins)
@@ -286,8 +316,32 @@ class WavefrontControl:
         
         if subtract_reference:
             self.slopes = (np.array([slopes_x, slopes_y]).flatten() - self.wfs_reference).ravel()
+            #self.slopes = (np.array([slopes_x, slopes_y]).flatten()).ravel() #- self.wfs_reference).ravel()
         else:
             self.slopes = (np.array([slopes_x, slopes_y]).flatten()).ravel()
+        
+        #mask = np.abs(self.slopes) > 0.001
+        #self.slopes[mask] = 0
+
+        if plot and self.count%50 == 0:
+            D=1
+            mid=int(self.slopes.shape[0]/2)
+            pyramid_plot_grid = make_pupil_grid(self.pixels, self.diameter) #hardcoded for now/ease
+            pyr_mask=circular_aperture(D)(pyramid_plot_grid)
+            sx = pyr_mask.copy()
+            sy = pyr_mask.copy()
+            sx[sx>0]=self.slopes[0:mid]
+            sy[sy>0]=self.slopes[mid::]
+            sxy=np.asarray([sx, sy]).reshape((2,self.pixels,self.pixels))
+            plt.clf()
+            plt.imshow(sxy.reshape((2*self.pixels, self.pixels)).transpose())
+            plt.colorbar()
+            plt.show()
+            plt.clf()
+        
+        mask = np.abs(self.slopes) > 0.001
+        self.slopes[mask] = 0
+
         return self.slopes
 
     def _sense_wavefront_modulated_pyramid(self):
@@ -299,9 +353,14 @@ class WavefrontControl:
 
     def _update_control_integrator(self):
         """ Basic integrator control method. """
-        #self.deformable_mirror.actuators = 
-        self.leak*self.deformable_mirror.actuators - self.gain*self.command_matrix.reshape((100, 632)).dot(self.slopes.reshape((632, 1)))
 
+        #self.deformable_mirror.actuators = 
+        #self.leak*self.deformable_mirror.actuators.reshape(100,1) - self.gain*self.command_matrix.reshape((100, 632)).dot(self.slopes.reshape((632, 1)))
+        #print(np.shape(self.deformable_mirror.actuators))
+        #self.deformable_mirror.actuators = (self.leak*self.deformable_mirror.actuators.reshape(self.n_actuators**2,1) - self.gain*self.command_matrix.reshape((self.n_actuators**2, 632)).dot(self.slopes.reshape((632, 1)))).ravel()
+        self.deformable_mirror.actuators = (self.leak*self.deformable_mirror.actuators.reshape(self.n_actuators**2,1) - self.gain*self.command_matrix.dot(self.slopes.reshape((632, 1)))).ravel()
+        #self.deformable_mirror.actuators = (self.leak*self.deformable_mirror.actuators.reshape(self.n_actuators**2,1) - self.gain*self.command_matrix.reshape((self.n_actuators**2, 632)).dot(np.zeros_like(self.slopes.reshape((632, 1))))).ravel()
+        #print(np.shape(self.gain*self.command_matrix.reshape((100, 632)).dot(self.slopes.reshape((632, 1)))))
 
 if __name__ == "__main__":
     wfc = WavefrontControl()
